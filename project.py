04ec4d4b4f18c52a7c70c22260f777d171328b5c
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-06-02 06:33:21 -0500

    THis is first lession of data scince of python

---
+++ project.py
@@ -0,0 +1,11 @@
+genres_tuple = ("pop", "rock", "soul", "hard rock", "soft rock", \
+                "R&B", "progressive rock", "disco") 
+C_tuple=(-5,1,-3)
+C_tuple1=sorted(C_tuple)
+print(C_tuple)
+print(C_tuple1)
+print(genres_tuple)
+print(genres_tuple[3])
+print(genres_tuple[3:6])
+print(genres_tuple[0:2])
+print(genres_tuple.index("disco"))
