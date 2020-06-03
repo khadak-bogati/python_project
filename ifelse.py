82a989f503782f2ed3409c9e92cb7588dedb6df8
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-06-03 18:26:22 -0500

    ifelse example

---
+++ ifelse.py
@@ -0,0 +1,79 @@
+
+#if statement example
+age = 20
+#Within an ident, we have the expression that is run if the condition is true
+if age > 19:
+	print('Congratulation you are good for go')
+#The statements after the if statement will run regradless if the condition is true or false
+print('move on')
+
+
+output
+...................
+Congratulation you are good for go
+move on
+
+
+...............................................................................................
+#Else statement Example
+age = 18
+#age =19
+
+if age > 18:
+	print('you can enter')
+else:
+	print('go see meat loaf')
+print('move on')
+
+
+...................................
+#Else statement Example
+age = 18
+#age =19
+
+if age > 18:
+	print('you can enter')
+else:
+	print('go see meat loaf')
+print('move on')
+
+
+out put 
+.................
+go see meat loaf
+move on
+
+
+
+.........................................
+#Condition statement Example
+album_year = 1983
+album_year = 1970
+
+if album_year > 1980:
+	print("album_year is greater than 1980")
+print("Do something ........")
+
+output
+...............
+Do something ........
+
+
+.........................................................
+#Condition statement Example
+album_year = 1983
+album_year = 1970
+
+if album_year > 1980:
+	print("album_year is greater than 1980")
+else:
+	print('Less then 1980')
+print("Do something ........")
+
+output
+.......................
+Less then 1980
+Do something ........
+
+
+..........................................................
