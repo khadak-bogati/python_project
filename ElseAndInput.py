0baaa1fecaa173eb7aee337385d14c7a6f2bf856
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-06-30 22:03:16 -0500

    Update ElseAndInput.py

--- ElseAndInput.py
+++ ElseAndInput.py
@@ -6,3 +6,18 @@ else:
 	for line in inputFile:
 		print(line)
 	inputFile.close()
+
+	
+	.......................................................
+	
+	
+inputFile = open("NumberFile.txt", mode = "r")
+try:
+	for line in inputFile:
+		print(int(line))
+except ValueError as error:
+	print("A value error occureed!")
+else:
+	print("No errors occureed! ")
+finally:
+	inputFile.close()
