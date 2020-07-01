86e7318fd0756089021d77854b094a0c281d3097
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-06-30 22:28:06 -0500

    Update ElseAndInput.py

--- ElseAndInput.py
+++ ElseAndInput.py
@@ -21,3 +21,20 @@ else:
 	print("No errors occureed! ")
 finally:
 	inputFile.close()
+
+	
+	
+	...................................................................................
+try:
+	inputFile = open("NumberFile.txt", mode = "r")
+	try:
+		for line in inputFile:
+			print(int(line))
+	except ValueError as error:
+		print("A value error occurred!")
+	else:
+		print("No errors occurred converting the file! ")
+	finally:
+		inputFile.close()
+except IOError as error:
+	print("An error occurred reading the file!")
