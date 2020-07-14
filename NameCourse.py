690c470bb440aa4cfe5d824cff36ceb9478da420
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-14 16:33:01 -0500

    Create NameCourse.py

---
+++ NameCourse.py
@@ -0,0 +1,8 @@
+classes = {"Math": ["David", "Lucy", "Dana"],
+			"Physics": ["Addison", "Vrushali", "Bilbo"],
+			"Chimisty": ["Sara", "Lugos", "Mireia", "Perle"],
+			"Computing": ["Partha", "Venijamin", "Terra", "Sofia"],
+			"History": ["Tryphon", "Govorg", "Raza", "Rein"]}
+print("Students in Computer Science:",classes["Computing"])
+classes["History"].append("Francis")
+print("Studens in History: ", classes["History"])
