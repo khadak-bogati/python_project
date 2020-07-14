4eacf3996668d0155281442cc056316d8e86d215
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-07-13 19:47:17 -0500

    Update SimpleGame.py

--- SimpleGame.py
+++ SimpleGame.py
@@ -22,3 +22,31 @@ verb_dict = {
 
 while True:
   get_input()
+  
+  
+  
+  
+  ===========================================
+  class GameObject:
+  class_name = ""
+  desc = ""
+  objects = {}
+
+  def __init__(self, name):
+    self.name = name
+    GameObject.objects[self.class_name] = self
+
+  def get_desc(self):
+    return self.class_name + "\n" + self.desc
+
+class Goblin(GameObject):
+  class_name = "goblin"
+  desc = "A foul creature"
+
+goblin = Goblin("Gobbly")
+
+def examine(noun):
+  if noun in GameObject.objects:
+    return GameObject.objects[noun].get_desc()
+  else:
+    return "There is no {} here.".format(noun)
